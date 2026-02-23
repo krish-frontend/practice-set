7e6e55cede5339e08d5e7aa3fe5af87b80707947
Krissh <krrishkumar610@gmail.com>
2026-02-23 23:31:06 +0530

    updated function file

---
+++ ch_3_loop/function.js
@@ -0,0 +1,14 @@
+
+
+function oneplusAvg(x,y){
+    return 1+(x+y)/2
+    console.log("done")
+}
+
+let a=2;
+let b=4;
+let c=5;
+
+console.log("avg 1", oneplusAvg(a,b))
+console.log("avg 2", oneplusAvg(b,c))
+console.log("avg 3", oneplusAvg(c,a))
